a3aeb84b299fe6427893ef53b60e52125adbe20c
LIU <liuyichen004@deppon.com>
2020-07-23 10:36:22 +0800

    0723

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ const path = require('path');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const resolve = (dir) => path.join(__dirname, dir);
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/site/vue-demo/' : '/',  // 公共路径
+  publicPath: process.env.NODE_ENV === '/',  // 公共路径
   indexPath: 'index.html', // 相对于打包路径index.html的路径
   outputDir: process.env.outputDir || 'dist', // 'dist', 生产环境构建文件的目录
   assetsDir: 'static', // 相对于outputDir的静态资源(js、css、img、fonts)目录
